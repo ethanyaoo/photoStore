44e212407f3de440e150cd7357c78f47cb34f635
ethan <ey709@nyu.edu>
2020-12-11 00:36:35 -0500

    Edited welcome.php

--- src/pages/welcome.php
+++ src/pages/welcome.php
@@ -1,8 +1,32 @@
+<?php
+
+	include "../config.php";
+
+	$query = $db->query("SELECT * FROM images ORDER by uploaded_on DESC");
+
+	if ($query->num_rows > 0)
+	{
+		while($row = $query->fetch_assoc())
+		{
+			$imaageURL = 'uploads/'.$row["file_name"];
+	?>
+		<img src="<?php echo $imageURL; ?>" alt="" />
+	<?php }
+	}
+	else
+	{
+		?>
+		<p>No image(s) found...</p>
+<?php } ?>
+
+
 <?php include "templates/header.php"; ?>
     <head>
       <title>Welcome </title>
    </head>
    
+	<img src="<?php echo $imageURL; ?>" alt="" />
+
    <body>
       <h1>Welcome <?php echo $login_session; ?></h1>
       <ul>
