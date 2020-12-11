ca50d795fcb4fe532e30af0dad2879dad5583bdf
ethan <ey709@nyu.edu>
2020-12-11 00:19:07 -0500

    Added upload.php

--- src/pages/upload.php
+++ src/pages/upload.php
@@ -3,7 +3,6 @@
 	{
 		$errors = array();
 		$file_name = $_FILES['image']['name'];
-		$file_size = $_FILES['image']['size'];
 		$file_tmp = $_FILES['image']['tmp_name'];
 		$file_type = $_FILES['image']['type'];
 		$file_ext = strtolower(end(explode('.', $_FILES['image']['name'])));
@@ -13,6 +12,16 @@
 		if (in_array($file_ext, $extensions) === false)
 		{
 			$errors[] = "Please choose a JPEG or PNG file";
+		}
+
+		if (empty($errors)==true)
+		{
+			move_uploaded_file($file_tmp, "images/".$file_name);
+			echo "Uploaded";
+		}
+		else
+		{
+			print_r($errors);
 		}
 	}
 ?>
