ce944ce50df068db7f733556bbfeb9a063cdfa53
ethan <ey709@nyu.edu>
2020-12-11 00:09:16 -0500

    fixed issues

--- src/install.php
+++ src/install.php
@@ -4,8 +4,8 @@
 
 	try
 	{
-		$connection = new PDO("mysql:host=$host", $username, $password, $options);
-		$sql = file_get_contents("../data/photoStore.sql");
+		$connection = new PDO("mysql:host=127.0.0.1", $username, $password, $options);
+		$sql = file_get_contents("data/photoStore.sql");
 		$connectionn->exec($sql);
 
 		echo "Database and tables created";
