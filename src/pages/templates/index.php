ed6d59e61b4a8fbd1ec1ed7f0247db9387784297
Indra-Ratna <46340853+Indra-Ratna@users.noreply.github.com>
2020-12-10 20:31:39 -0800

    Add files via upload

---
+++ src/pages/templates/index.php
@@ -0,0 +1,16 @@
+<?php include "templates/header.php"; ?>
+
+    <<head>
+        <title>PhotoStore</title>
+    </head>
+    <body>
+        <h1>This is PhotoStore!</h1>
+        <div>
+            <ul>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="register.php">Register</a></li>
+            </ul>
+        </div>
+    </body>
+
+<?php include "templates/footer.php"; ?>
