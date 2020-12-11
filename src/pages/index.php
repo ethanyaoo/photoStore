9c575b17e6a8d7d53623d324dd2e7c1bee2004c6
ethan <ey709@nyu.edu>
2020-12-10 21:46:41 -0500

    Initial commit

---
+++ src/pages/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="x-ua-compatible" content="ie=edge" />
+		<meta name="viewport" content = "width=device-width, initial-scale=1" />
+
+		<title> Login or Register </title>
+
+	</head>
+
+	<body>
+		<h1> Login or Register </h1>
+	
+		<ul>
+			<li>
+				<a href = "create.php">
+					<strong>
+						Register
+					</strong>
+				</a>
+			</li>
+			<li>
+				<a href="login.php">
+					<strong>
+						Login
+					</strong>
+				</a>
+			</li>
+		</ul>
+	</body>
+</html>
