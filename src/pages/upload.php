f135334e1d093d5439cbff2e71818931f556feae
ethan <ey709@nyu.edu>
2020-12-10 23:58:38 -0500

    fixed issues

--- src/pages/upload.php
+++ src/pages/upload.php
@@ -1,1 +1,37 @@
+<?php
+	if(isset($_FILES['image']))
+	{
+		$errors = array();
+		$file_name = $_FILES['image']['name'];
+		$file_size = $_FILES['image']['size'];
+		$file_tmp = $_FILES['image']['tmp_name'];
+		$file_type = $_FILES['image']['type'];
+		$file_ext = strtolower(end(explode('.', $_FILES['image']['name'])));
+
+		$extensions = array("jpeg", "jpg", "png");
+
+		if (in_array($file_ext, $extensions) === false)
+		{
+			$errors[] = "Please choose a JPEG or PNG file";
+		}
+	}
+?>
+
 <?php include "templates/header.php"; ?>
+
+<h2>
+	<form action="" method="POST" enctype="multipart/form-data">
+		<label for="file">
+			Upload an image
+		</label>
+
+		<input type="file" name="image" />
+
+		<label for="submit">
+			Submit
+		</label>
+		<input type="submit"/>
+	</form>
+</h2>
+
+<?php include "templates/footer.php"; ?>
