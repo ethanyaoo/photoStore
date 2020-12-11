15fa75e29f199f1f10bddf11b51b52b45bd1b526
ethan <ey709@nyu.edu>
2020-12-10 23:13:36 -0500

    edited register page and attached pdo connection

--- src/pages/create.php
+++ src/pages/create.php
@@ -1,3 +1,36 @@
+if (isset($_POST['submit']))
+{
+	require "../config.php";
+	
+	try
+	{
+		$connection = new PDO($dsn, $username, $password, $options);
+
+		$new_user = array(
+			"username" => $_POST['username'],
+			"password" => $_POST['password'],
+			"firstName" => $_POST['firstName'],
+			"lastName" => $_POST['lastName'],
+			"bio"	   => $_POST['bio']
+		);
+
+		$sql = sprintf(
+			"INSERT INTO %s (%s) values (%s)",
+			"users",
+			implode(", ", array_keys($new_user)),
+			":" . implode(", :", array_keys($new_user))
+			);
+
+		$statement = $connection->prepare($sql);
+		$statement->execute($new_user);
+
+	{
+	catch(PDOException $error)
+	{
+		echo $sql . "<br>" . $error->getMessage();
+	}
+}
+
 <?php include "templates/header.php"; ?>
 
 <h2> Register new account </h2>
@@ -5,14 +38,28 @@
 		<label for="username">
 			Username
 		</label>
-		
 		<input type="text" name="username" id="username">
 
 		<label for="password">
 			Password
 		</label>
-
 		<input type="text" name="password" id="password">
+
+		<label for="firstName">
+			First Name
+		</label>
+		<input type="text" name="firstName" id="firstName">
+
+		<label for="lastName">
+			Last Name
+		</label>
+		<input type="text" name="lastName" id="lastName">
+
+		<label for="bio">
+			Bio
+		</label>
+		<input type="text" name="bio" id="bio">
+
 
 		<input type="submit" name="submit" value="Submit">
 	</form>
